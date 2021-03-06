81af338cd9c70763931de4d279bc25b3e8a2d9fb
Esteban Borai <estebanborai@gmail.com>
2021-03-06 19:05:56 -0300

    feat: setup actix entrypoint

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-systemfd --no-pid -s http::0.0.0.0:7878 -- cargo watch -x run
+cargo watch -x run
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate log;
+
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/{id}/{name}")]
+async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
+    format!("Hello {}! id:{}", name, id)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    if cfg!(debug_assertions) {
+        dotenv::from_filename(".env.development")
+            .expect("Unable to find \".env.development\" in the CWD");
+    }
+
+    env_logger::init();
+
+    info!("Serving on http://0.0.0.0:7878");
+    HttpServer::new(|| App::new().service(index))
+        .bind("0.0.0.0:7878")?
+        .run()
+        .await
 }
