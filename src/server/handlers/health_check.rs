a3726482f93c939f2d708418cc08e02d4819f226
Luis Moreno <morenol@users.noreply.github.com>
2021-03-15 23:38:38 -0400

    Refactor health_check

--- src/server/handlers/health_check.rs
+++ src/server/handlers/health_check.rs
@@ -4,32 +4,17 @@ use serde::Serialize;
 use crate::state::State;
 
 pub async fn health_check(state: web::Data<State>) -> HttpResponse {
-    let db_status = sqlx::query("SELECT 1")
+    match sqlx::query("SELECT 1")
         .fetch_one(state.db_pool.as_ref())
         .await
-        .is_ok();
-    let service_status = ServiceStatus::new(db_status);
-
-    service_status.into()
+    {
+        Ok(_) => HttpResponse::Ok().json(ServiceStatus { db_status: true }),
+        Err(_) => HttpResponse::InternalServerError().json(ServiceStatus { db_status: false }),
+    }
 }
 
 /// Service Health Check
 #[derive(Serialize)]
 struct ServiceStatus {
     db_status: bool,
-}
-
-impl ServiceStatus {
-    pub fn new(db_status: bool) -> Self {
-        Self { db_status }
-    }
-}
-
-impl From<ServiceStatus> for HttpResponse {
-    fn from(status: ServiceStatus) -> HttpResponse {
-        match status.db_status {
-            true => HttpResponse::Ok().json(status),
-            false => HttpResponse::InternalServerError().json(status),
-        }
-    }
 }
