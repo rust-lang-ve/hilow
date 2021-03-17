fff2bf3de97754f82a41f023db3a71adda45f8c6
Luis Moreno <morenol@users.noreply.github.com>
2021-03-16 20:15:23 -0400

    Use actix test suite

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,47 +1,36 @@
-use std::net::TcpListener;
+use actix_web::{http::StatusCode, test, web::Data, App};
 
-use anyhow::Result;
-
-use hilow;
+use hilow::state::State;
 
 #[actix_rt::test]
 async fn health_check_works() {
-    let address = spawn_app().expect("Unable to spawn server");
-    let client = reqwest::Client::new();
+    std::env::set_var("DATABASE_URL", "postgres://hilow:hilow@127.0.0.1/hilow");
+    let state = State::new().expect("Failed to initialze \"State\"!");
+    let state = Data::new(state);
 
-    let response = client
-        .get(&format!("{}/health_check", address))
-        .send()
-        .await
-        .expect("Failed to  execute request.");
+    let mut app =
+        test::init_service(App::new().app_data(state).configure(hilow::server::router)).await;
 
-    assert!(response.status().is_success());
+    let req = test::TestRequest::get().uri("/health_check").to_request();
+
+    let resp = test::call_service(&mut app, req).await;
+    assert_eq!(resp.status(), StatusCode::OK);
 }
 
 #[actix_rt::test]
 async fn health_check_works_failure() {
+    // Set wrong password in order to produce health_check error
     std::env::set_var("DATABASE_URL", "postgres://hilow:h@127.0.0.1/hilow");
-    let address = spawn_app().expect("Unable to spawn server");
 
-    let client = reqwest::Client::new();
+    let state = State::new().expect("Failed to initialze \"State\"!");
+    let state = Data::new(state);
 
-    let response = client
-        .get(&format!("{}/health_check", address))
-        .send()
-        .await
-        .expect("Failed to  execute request.");
+    let mut app =
+        test::init_service(App::new().app_data(state).configure(hilow::server::router)).await;
 
-    assert!(response.status().is_server_error());
-}
+    let req = test::TestRequest::get().uri("/health_check").to_request();
 
-/// Spawn a hilow server and return its address to the caller.
-fn spawn_app() -> Result<String> {
-    // Port 0 translates to a random port.
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    // We retrieve the port assigned to us by the OS
-    let port = listener.local_addr().unwrap().port();
-    let server = hilow::server::run(listener).unwrap();
-    let _ = tokio::spawn(server);
+    let resp = test::call_service(&mut app, req).await;
 
-    Ok(format!("http://127.0.0.1:{}", port))
+    assert_eq!(resp.status(), StatusCode::INTERNAL_SERVER_ERROR);
 }
