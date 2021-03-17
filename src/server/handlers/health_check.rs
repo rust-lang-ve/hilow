7d1641a8a4bed9cebdd2d98f918fe8a7d891b633
Luis Moreno <morenol@users.noreply.github.com>
2021-03-17 19:45:14 -0400

    Rename ServiceStatus struct to HealtCheckResponse

--- src/server/handlers/health_check.rs
+++ src/server/handlers/health_check.rs
@@ -3,18 +3,18 @@ use serde::Serialize;
 
 use crate::state::State;
 
+/// Service Health Check
+#[derive(Serialize)]
+struct HealtCheckResponse {
+    db_status: bool,
+}
+
 pub async fn health_check(state: web::Data<State>) -> HttpResponse {
     match sqlx::query("SELECT 1")
         .fetch_one(state.db_pool.as_ref())
         .await
     {
-        Ok(_) => HttpResponse::Ok().json(ServiceStatus { db_status: true }),
-        Err(_) => HttpResponse::InternalServerError().json(ServiceStatus { db_status: false }),
+        Ok(_) => HttpResponse::Ok().json(HealtCheckResponse { db_status: true }),
+        Err(_) => HttpResponse::InternalServerError().json(HealtCheckResponse { db_status: false }),
     }
-}
-
-/// Service Health Check
-#[derive(Serialize)]
-struct ServiceStatus {
-    db_status: bool,
 }
